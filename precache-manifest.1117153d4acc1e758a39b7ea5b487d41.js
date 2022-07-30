e3ce35f8249e6d933b5d467934b7343fefb246a5
MOHD AKRAM ANSARI <61919643+ansariakram82@users.noreply.github.com>
2022-07-30 22:54:06 +0530

    Add files via upload

---
+++ precache-manifest.1117153d4acc1e758a39b7ea5b487d41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d7858f60d363d6c4c80842d1e6447b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b3edfdad028795114d",
+    "url": "/static/css/main.a7682a60.chunk.css"
+  },
+  {
+    "revision": "b056f4a4e67dfa3c892e",
+    "url": "/static/js/2.5f84a77e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5f84a77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b3edfdad028795114d",
+    "url": "/static/js/main.e1a25a1b.chunk.js"
+  },
+  {
+    "revision": "b58892f40f76d3ffeb1a",
+    "url": "/static/js/runtime-main.4b6d85fc.js"
+  }
+]);
